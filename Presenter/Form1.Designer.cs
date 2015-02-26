67416290259e6e4f3876100628b2d53553811e59
Hoang Son <wingadium1@gmail.com>
2015-02-25 18:45:14 -0800

    init

---
+++ Presenter/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Presenter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.labelAnswer = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.process1 = new System.Diagnostics.Process();
+            this.timerCountDown = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1350, 729);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.Tag = "";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.labelAnswer);
+            this.tabPage2.Controls.Add(this.labelStatus);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Controls.Add(this.labelTimer);
+            this.tabPage2.Controls.Add(this.labelQuestion);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tabPage2.Size = new System.Drawing.Size(1342, 703);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Question";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // labelAnswer
+            // 
+            this.labelAnswer.AutoSize = true;
+            this.labelAnswer.Font = new System.Drawing.Font("MS Reference Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAnswer.Location = new System.Drawing.Point(8, 606);
+            this.labelAnswer.Name = "labelAnswer";
+            this.labelAnswer.Size = new System.Drawing.Size(95, 34);
+            this.labelAnswer.TabIndex = 4;
+            this.labelAnswer.Text = "label2";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(8, 685);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(35, 13);
+            this.labelStatus.TabIndex = 3;
+            this.labelStatus.Text = "label2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(383, 155);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 400);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Dock = System.Windows.Forms.DockStyle.Right;
+            this.labelTimer.ForeColor = System.Drawing.Color.Red;
+            this.labelTimer.Location = new System.Drawing.Point(1304, 3);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(35, 13);
+            this.labelTimer.TabIndex = 1;
+            this.labelTimer.Text = "label2";
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Font = new System.Drawing.Font("MS Reference Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelQuestion.Location = new System.Drawing.Point(8, 49);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(95, 34);
+            this.labelQuestion.TabIndex = 0;
+            this.labelQuestion.Text = "label2";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1342, 703);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "Answer";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // timerCountDown
+            // 
+            this.timerCountDown.Tick += new System.EventHandler(this.timerCountDown_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.tabControl1);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label labelAnswer;
+        private System.Windows.Forms.Timer timerCountDown;
+
+    }
+}
+
---
+++ Presenter/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Presenter
+{
+    public partial class Form1 : Form
+    {
+        #region Variable
+
+        //defaut address
+        string IP = null;
+        int PORT = 2505;
+
+        //Threads
+        System.Threading.Thread thConnecttoServer;
+
+        //Biến dùng để gửi, nhận dữ liệu
+        byte[] inputData = new byte[1024];
+
+        TcpClient _client;
+        public TcpClient Client
+        {
+            get { return _client; }
+            set { _client = value; }
+        }
+        
+        private int timeLeft;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            inputIP();
+        }
+
+        private void inputIP()
+        {
+            InputBoxResult test = InputBox.Show("Input server IP" + "\n" 
+                  , "Input server IP", "Default", 100, 0);
+
+            if (test.ReturnCode == DialogResult.OK)
+            {
+                IP = test.Text;
+                // MessageBox.Show(IP);
+
+                thConnecttoServer = new Thread(Connect);
+                thConnecttoServer.IsBackground = true;
+                thConnecttoServer.Start();
+            }
+        }
+
+        private delegate void dlgStartTheQuestion();
+        private void Connect()
+        {
+            try
+            {
+
+                Client = new TcpClient();
+                Client.Connect(IPAddress.Parse(IP), PORT);
+
+                while (true)//Trong khi vẫn còn kết nối
+                {
+                    //Nhận dữ liệu từ máy chủ
+                    try
+                    {
+                        labelStatus.Text = "Connected to server IP:  " + IP;
+                            byte[] dlNhan = new byte[1048576];
+                            Client.GetStream().Read(dlNhan, 0, 1048576);
+
+                            // tmp = Encoding.Unicode.GetString(dlNhan);
+                            Utility.Message reciveMessage = ByteArrayToMessage(dlNhan);
+                            switch (reciveMessage.type)
+                            {
+                                case (Utility.Message.Type.Quest):
+                                    labelQuestion.Text = reciveMessage.x.question;
+                                    labelAnswer.Text = "The true answer is : " + reciveMessage.x.ans;
+                                    labelAnswer.Visible = false;
+                                    if (null != reciveMessage.image)
+                                    {
+                                        MemoryStream ms = new MemoryStream(reciveMessage.image);
+                                        Image returnImage = Image.FromStream(ms);
+                                        Bitmap image = null;
+                                        image = new Bitmap(returnImage);
+                                        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                                        pictureBox1.ClientSize = new Size(600, 400);
+                                        pictureBox1.Image = (Image)image;
+                                    }
+                                    {
+
+                                        StartTheQuestion();
+                                    }
+                                    break;
+                                case (Utility.Message.Type.ShowAns):
+                                    labelAnswer.Visible = true;
+                                    break;
+                            }
+                        }
+                    
+
+                    catch
+                    {
+                        MessageBox.Show("Lost Connect!!!! + IP");
+                        Connect();
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Lost Connect!!!!");
+                Connect();
+            }
+
+        }
+        private void SendData(Utility.Message data)
+        {
+            try
+            {
+                System.IO.MemoryStream fs = new System.IO.MemoryStream();
+                System.Runtime.Serialization.Formatters.Binary.BinaryFormatter formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                formatter.Serialize(fs, data);
+                byte[] buffer = fs.ToArray();
+                Client.GetStream().Write(buffer, 0, buffer.Length);
+
+            }
+            catch (Exception er)
+            {
+
+
+            }
+
+        }
+
+        private string LocalIPAddress()
+        {
+            IPHostEntry host;
+            string localIP = "";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            return localIP;
+        }
+
+        private byte[] MessageToByteArray(Utility.Message message)
+        {
+            System.IO.MemoryStream fs = new System.IO.MemoryStream();
+            System.Runtime.Serialization.Formatters.Binary.BinaryFormatter formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            formatter.Serialize(fs, message);
+            return fs.ToArray();
+        }
+
+        private static Utility.Message ByteArrayToMessage(byte[] arrBytes)
+        {
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+
+            System.Runtime.Serialization.Formatters.Binary.BinaryFormatter formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            ms.Write(arrBytes, 0, arrBytes.Length);
+            ms.Seek(0, System.IO.SeekOrigin.Begin);
+            Utility.Message reciveMessage = (Utility.Message)formatter.Deserialize(ms);
+            return reciveMessage;
+        }
+
+        delegate void StartQuestionDelegate();
+        private void StartTheQuestion()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new StartQuestionDelegate(StartTheQuestion));
+            }
+            else
+            {
+                timeLeft = 100;
+                labelTimer.Text = "10''00";
+                timerCountDown.Interval = 100;
+                timerCountDown.Start();
+            }
+        }
+
+        delegate void SetTextCallback(string text);
+        public void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.labelTimer.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.labelTimer.Text = text;
+            }
+        }
+
+        private void timerCountDown_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                // Display the new time left 
+                // by updating the Time Left label.
+                timeLeft -= 1;
+                labelTimer.Text = String.Format("{0}''{1}", timeLeft / 10, (timeLeft % 10));
+            }
+            else
+            {
+                // If the user ran out of time, stop the timer, show  and fill in the answers.
+                timerCountDown.Stop();
+                labelTimer.Text = "Time's up!";
+                // MessageBox.Show("Time's up");
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Client.Close();
+            thConnecttoServer.Abort();
+            Application.Exit();
+        }
+
+    }
+}

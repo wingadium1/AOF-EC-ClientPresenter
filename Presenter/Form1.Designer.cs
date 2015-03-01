89a9d8a2e5f62061dffed174f1d2f822310f1f9a
Hoang Son <wingadium1@gmail.com>
2015-02-28 16:37:22 -0800

    realease 1.0a

--- Presenter/Form1.Designer.cs
+++ Presenter/Form1.Designer.cs
@@ -37,6 +37,15 @@
             this.labelTimer = new System.Windows.Forms.Label();
             this.labelQuestion = new System.Windows.Forms.Label();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label8 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lbAns4 = new System.Windows.Forms.Label();
+            this.lbAns3 = new System.Windows.Forms.Label();
+            this.lbAns2 = new System.Windows.Forms.Label();
+            this.lbAns1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.process1 = new System.Diagnostics.Process();
             this.timerCountDown = new System.Windows.Forms.Timer(this.components);
@@ -104,11 +113,11 @@
             // labelTimer
             // 
             this.labelTimer.AutoSize = true;
-            this.labelTimer.Dock = System.Windows.Forms.DockStyle.Right;
+            this.labelTimer.Font = new System.Drawing.Font("Microsoft NeoGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelTimer.ForeColor = System.Drawing.Color.Red;
-            this.labelTimer.Location = new System.Drawing.Point(1304, 3);
+            this.labelTimer.Location = new System.Drawing.Point(1252, 3);
             this.labelTimer.Name = "labelTimer";
-            this.labelTimer.Size = new System.Drawing.Size(35, 13);
+            this.labelTimer.Size = new System.Drawing.Size(52, 21);
             this.labelTimer.TabIndex = 1;
             this.labelTimer.Text = "label2";
             // 
@@ -124,6 +133,15 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Controls.Add(this.lbAns4);
+            this.tabPage1.Controls.Add(this.lbAns3);
+            this.tabPage1.Controls.Add(this.lbAns2);
+            this.tabPage1.Controls.Add(this.lbAns1);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
@@ -133,12 +151,99 @@
             this.tabPage1.Text = "Answer";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(1141, 636);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(143, 44);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "label8";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 636);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1040, 59);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // lbAns4
+            // 
+            this.lbAns4.AutoSize = true;
+            this.lbAns4.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAns4.Location = new System.Drawing.Point(79, 409);
+            this.lbAns4.Name = "lbAns4";
+            this.lbAns4.Size = new System.Drawing.Size(143, 44);
+            this.lbAns4.TabIndex = 7;
+            this.lbAns4.Text = "label7";
+            // 
+            // lbAns3
+            // 
+            this.lbAns3.AutoSize = true;
+            this.lbAns3.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAns3.Location = new System.Drawing.Point(79, 289);
+            this.lbAns3.Name = "lbAns3";
+            this.lbAns3.Size = new System.Drawing.Size(144, 44);
+            this.lbAns3.TabIndex = 6;
+            this.lbAns3.Text = "label6";
+            // 
+            // lbAns2
+            // 
+            this.lbAns2.AutoSize = true;
+            this.lbAns2.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAns2.Location = new System.Drawing.Point(79, 169);
+            this.lbAns2.Name = "lbAns2";
+            this.lbAns2.Size = new System.Drawing.Size(142, 44);
+            this.lbAns2.TabIndex = 5;
+            this.lbAns2.Text = "label5";
+            // 
+            // lbAns1
+            // 
+            this.lbAns1.AutoSize = true;
+            this.lbAns1.Font = new System.Drawing.Font("Perpetua Titling MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAns1.Location = new System.Drawing.Point(79, 49);
+            this.lbAns1.Name = "lbAns1";
+            this.lbAns1.Size = new System.Drawing.Size(142, 44);
+            this.lbAns1.TabIndex = 4;
+            this.lbAns1.Text = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 363);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Location = new System.Drawing.Point(8, 3);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(70, 25);
             this.label1.TabIndex = 0;
@@ -192,6 +297,15 @@
         private System.Windows.Forms.Label labelStatus;
         private System.Windows.Forms.Label labelAnswer;
         private System.Windows.Forms.Timer timerCountDown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbAns1;
+        private System.Windows.Forms.Label lbAns2;
+        private System.Windows.Forms.Label lbAns3;
+        private System.Windows.Forms.Label lbAns4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label8;
 
     }
 }
